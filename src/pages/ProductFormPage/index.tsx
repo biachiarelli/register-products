75498ae2cd93fcec069cae9b6b0b39c15af214e9
Beatriz Chiarelli <biachiarelli@gmail.com>
2025-04-13 14:55:56 -0300

    Layout form page

--- src/pages/ProductFormPage/index.tsx
+++ src/pages/ProductFormPage/index.tsx
@@ -1,12 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './index.scss';
 import { useEffect } from 'react';
 import { Product } from '../../models/Product';
 import api from '../../services/api';
 import { AxiosResponse } from 'axios';
+import { IconButton } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function ProductFormPage() {
+  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>(); 
+
+  const goToDeatils = () => {
+    navigate(`/product/${id}`);
+  }
 
   const getProductById = async () => {
     if (!id) return; 
@@ -28,8 +35,13 @@ export default function ProductFormPage() {
 
   return (
     <div className="product-form">
-      <div className="product-list-header">
-        <h1 className="product-list-header__title"> {id ? 'Editar produto' : 'Cadastrar novo produto'}</h1>
+      <div className="product-form-header">
+        { id && 
+          <IconButton color='secondary' onClick={() => goToDeatils()} >
+            <ArrowBackIcon />
+          </IconButton>
+        }
+        <h1 className="product-form-header__title"> {id ? 'Editar produto' : 'Cadastrar novo produto'}</h1>
       </div>
     </div>
   );
